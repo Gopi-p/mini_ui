2007ba0cb65df76ef58db4d8319d3ec0dc223006
Gopinath <gopinath.p@tandemloop.in>
2022-04-26 19:34:37 +0530

    Helpers updated

--- lib/src/shared/colors.dart
+++ lib/src/shared/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-/// Theme colors which can be overrided by the users
+/// Theme colors which can be override by the users
 const Color miniPrimaryDark = Color(0xff012a4a);
 const Color miniPrimaryLight = Color(0xff61a5c2);
 const Color miniSecondaryDark = Color(0xffe0c3fc);
--- lib/src/shared/helpers.dart
+++ lib/src/shared/helpers.dart
@@ -7,6 +7,7 @@ const Widget hlSpaceM = SizedBox(width: 18.0);
 const Widget hlSpaceL = SizedBox(width: 24.0);
 const Widget hlSpaceXL = SizedBox(width: 32.0);
 const Widget hlSpaceXXL = SizedBox(width: 50.0);
+Widget hlSpace(double width) => SizedBox(width: width);
 
 /// Vertical spaces using [SizedBox] widgets
 const Widget vlSpaceXS = SizedBox(height: 5.0);
@@ -15,6 +16,7 @@ const Widget vlSpaceM = SizedBox(height: 18.0);
 const Widget vlSpaceL = SizedBox(height: 24.0);
 const Widget vlSpaceXL = SizedBox(height: 31.0);
 const Widget vlSpaceXXL = SizedBox(height: 50.0);
+Widget vlSpace(double height) => SizedBox(height: height);
 
 /// Returns a screen width by using [context]
 double screenWidth(BuildContext context) => MediaQuery.of(context).size.width;
@@ -29,3 +31,16 @@ double screenHeightPercentage(BuildContext context, {double percentage = 1}) =>
 /// Returns a screen width in percentages
 double screenWidthPercentage(BuildContext context, {double percentage = 1}) =>
     screenWidth(context) * percentage;
+
+
+// extension TltColorInvertExtension on Color {
+//   Color tltInvert() {
+//     // Returning black or white based on the intensities of red, blue & green in the color.
+//     return (red * 0.299) + (green * 0.587) + (blue * 0.114) > 186
+//         ? TltColors.black
+//         : TltColors.white;
+
+//     // Inverting a color
+//     // Color.fromARGB((opacity * 255).round(), 255 - red, 255 - green, 255 - blue);
+//   }
+// }
